353eccdf7d3d0a5e82344b6163ba02d58c453f95
Alper Efe Şahin <efe.mate@gmail.com>
2025-02-15 18:03:27 +0300

    change 'App' name to 'AppWidget'

--- lib/core/init/app_widget.dart
+++ lib/core/init/app_widget.dart
@@ -3,8 +3,8 @@ import 'package:bare_bones_flutter/core/init/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class App extends StatelessWidget {
-  const App({super.key});
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:bare_bones_flutter/core/di/dependency_injector.dart';
-import 'package:bare_bones_flutter/core/init/app/app.dart';
+import 'package:bare_bones_flutter/core/init/app_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   injectionSetup();
 
-  runApp(const App());
+  runApp(const AppWidget());
 }
