f4de88a4bfd1a68e42fc37819e9e7e85b28b4a25
Alper Efe Şahin <efe.mate@gmail.com>
2025-02-16 13:06:05 +0300

    update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bare_bones_flutter/core/config/app_config.dart';
 import 'package:bare_bones_flutter/core/di/dependency_injector.dart';
 import 'package:bare_bones_flutter/core/init/app_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,7 +7,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final apiKey = AppConfig.apiKey;
+  final appId = AppConfig.appId;
+  final messagingSenderId = AppConfig.messagingSenderId;
+  final projectId = AppConfig.projectId;
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(apiKey: apiKey, appId: appId, messagingSenderId: messagingSenderId, projectId: projectId),
+  );
 
   injectionSetup();
 
